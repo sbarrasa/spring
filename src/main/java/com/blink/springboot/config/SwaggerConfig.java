75d1cfae8b7ff2524085c6a60b690c677db7500f
Sebastian Barrasa <sebastian.barrasa@blink-srl.com>
2022-11-25 15:26:50 -0300

    Swagger appi

--- src/main/java/com/blink/springboot/config/SwaggerConfig.java
+++ src/main/java/com/blink/springboot/config/SwaggerConfig.java
@@ -28,9 +28,9 @@ public class SwaggerConfig implements WebMvcConfigurer{
 
     private ApiInfo apiInfoMetaData() {
 
-        return new ApiInfoBuilder().title("NAME OF SERVICE")
-                .description("API Endpoint Decoration")
-                .contact(new Contact("Dev-Team", "https://www.dev-team.com/", "dev-team@gmail.com"))
+        return new ApiInfoBuilder().title("CRUD")
+                .description("En ejemplo de CRUD simple usando spring")
+                .contact(new Contact("Sebastián Barrasa", "https://www.cruzagramas.com.ar", "sebastian.barrasa@blink-srl.com"))
                 .license("Apache 2.0")
                 .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
                 .version("1.0.0")
