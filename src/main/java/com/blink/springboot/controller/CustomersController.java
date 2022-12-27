3fcc9290b1439a20a4886333efc75a44cb890877
Sebastian Barrasa <sebastian.barrasa@blink-srl.com>
2022-12-27 17:16:13 -0300

    Separación de los métodos de servicio de Customers del Controller

--- src/main/java/com/blink/springboot/controller/CustomersController.java
+++ src/main/java/com/blink/springboot/controller/CustomersController.java
@@ -4,7 +4,6 @@ package com.blink.springboot.controller;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import javax.websocket.server.PathParam;
 
@@ -12,61 +11,45 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.*;
-import org.springframework.data.domain.ExampleMatcher.StringMatcher;
-import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import com.blink.springboot.dao.CustomersRepository;
 import com.blink.springboot.entities.Customer;
 import com.blink.springboot.entities.Sex;
+import com.blink.springboot.services.CustomersManager;
 import com.blink.springboot.services.Server2;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomersController {
-	private final String defaultOrder = "id";
 
 	@Value("${kafka.topic.name}") 
 	private String kafkaTopic;
 	
 	@Autowired
 	private KafkaTemplate<String, Customer> kafkaTemplate;
-	
+
 	@Autowired
-	private CustomersRepository customersRepository;
+	private CustomersManager customersManager;
 
 	@Autowired
 	private Server2 server2 ;
 	
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	
-	 /*	@Autowired
-	private CustomerRedisRepository customersRedisRepository;
-*/	
-
 	
 	
 	@RequestMapping(path = "/all", method = RequestMethod.GET)
 	public Page<Customer> getAll(@RequestParam(required = false) Optional<Integer> page,
 			 				  @RequestParam(required = false) Optional<Integer> size,
-			 				  @RequestParam(required = false, defaultValue = defaultOrder  ) List<String> orderFields) {
+			 				  @RequestParam(required = false) List<String> orderFields) {
 		
-		if(orderFields.isEmpty())
-			orderFields.add(defaultOrder);
 		
-		return customersRepository.findAll(PageRequest.of( 
-										page.orElse(0), 
-										size.orElse(50),
-										Sort.by(orderFields.toArray(new String[0]))));
 		
-		//								Sort.by("lastNames", "names" )));
-
+		return customersManager.getPaginated(page, size, orderFields);
 		
+			
 	}
 
 	@GetMapping("/")
@@ -76,72 +59,41 @@ public class CustomersController {
 							  @RequestParam(required = false) Optional<Integer> ageFrom,
 							  @RequestParam(required = false) Optional<Integer> ageTo) {
 		
-		Customer customerQuery = new Customer() 
-				.setLastNames(lastNames) 
-				.setNames(names) 
-				.setSex(sex);
-		
-		Example<Customer> example = Example.of(customerQuery, 
-										ExampleMatcher.matching()
-											.withIgnoreCase(true)
-											.withStringMatcher(StringMatcher.CONTAINING));
-		
-		Sort sort = Sort.by("lastNames", "names");
-		
-		List<Customer> customers = customersRepository.findAll(
-									example, 
-									sort);
-		
-		return customers.stream()
-				.filter(customer -> Range.closed(ageFrom.orElse(0),
-			  						         ageTo.orElse(Integer.MAX_VALUE))
-											.contains(customer.getAge()))
-				.collect(Collectors.toList());
+		return customersManager.get(lastNames, names, sex, 
+										Range.closed(ageFrom.orElse(0),
+													 ageTo.orElse(Integer.MAX_VALUE)));
 		
 		
 	}
 
-	@Cacheable(value =  "Customer", key = "#id")
 	@GetMapping("/{id}")
 	public Customer getById(@PathVariable Long id) {
-		Customer customer = customersRepository.findById(id)
-							.orElseThrow();
-		
-		logger.info("Getting customer {}", customer);
-		
-		return customer;
+		return customersManager.get(id);
 	}
 
 	@PostMapping("/")
 	public Customer create(@RequestBody Customer customer) {
-		logger.info("Saving customer {}", customer);
-		
-		return customersRepository.save(customer);
+		return customersManager.save(customer)
+;
 	}
 
-	@CachePut(value =  "Customer", key = "#id")
 	@PutMapping("/{id}")
 	public Customer update(@PathVariable Long id, @RequestBody Customer customer){
 		customer.setId(id);
-		logger.info("Saving customer {}", customer);
-
-		return customersRepository.save(customer);	
+		
+		return customersManager.save(customer);
+		
 	}
 
-	@CacheEvict(value =  "Customer", key = "#id")
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Customer> delete(@PathVariable Long id){
-		Customer customer = customersRepository.findById(id)
-				.orElseThrow();
-		logger.info("Deleting customer {}", customer);
+	public Customer delete(@PathVariable Long id){
+		return customersManager.delete(id);
 		
-		customersRepository.delete(customer);
-		return ResponseEntity.ok(customer);
 	}
 	
 	@GetMapping("/sex")
 	public List<Customer> getBySex(@RequestParam Set<Sex> sexs) {
-		return customersRepository.findBySex(sexs);
+		return customersManager.get(sexs);
 	}
 	
 	@PutMapping("/kafka/{id}")
---
+++ src/main/java/com/blink/springboot/services/CustomersManager.java
@@ -0,0 +1,109 @@
+package com.blink.springboot.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Range;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.blink.springboot.dao.CustomersRepository;
+import com.blink.springboot.entities.Customer;
+import com.blink.springboot.entities.Sex;
+
+@Service
+public class CustomersManager {
+	@Autowired
+	private CustomersRepository customersRepository;
+
+	private final String defaultOrder = "id";
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	public List<Customer> get(String lastNames, 
+							  String names,
+							  Sex sex,
+							  Range<Integer> ages ){
+		
+		Customer customerQuery = new Customer() 
+				.setLastNames(lastNames) 
+				.setNames(names) 
+				.setSex(sex);
+		
+		Example<Customer> example = Example.of(customerQuery, 
+										ExampleMatcher.matching()
+											.withIgnoreCase(true)
+											.withStringMatcher(StringMatcher.CONTAINING));
+		
+		Sort sort = Sort.by("lastNames", "names");
+		
+		List<Customer> customers = customersRepository.findAll(
+									example, 
+									sort);
+		
+		return customers.stream()
+				.filter(customer -> ages.contains(customer.getAge()))
+				.collect(Collectors.toList());
+	}
+
+	@Cacheable(value =  "Customer", key = "#id")
+	public Customer get(Long id) {
+		Customer customer = customersRepository.findById(id)
+				.orElseThrow();
+
+		logger.info("Getting customer {}", customer);
+
+		return customer;
+	}
+
+	public Page<Customer> getPaginated(Optional<Integer> page, Optional<Integer> size, List<String> orderFields) {
+		if(orderFields.isEmpty())
+			orderFields.add(defaultOrder);
+	
+		return customersRepository.findAll(PageRequest.of( 
+				page.orElse(0), 
+				size.orElse(50),
+				Sort.by(orderFields.toArray(new String[0]))));
+	}
+
+	@CachePut(value =  "Customer", key = "#id")
+	public Customer save(Customer customer) {
+		logger.info("Saving customer {}", customer);
+		
+		return customersRepository.save(customer);
+	}
+
+	public Customer delete(Long id) {
+		Customer customer = customersRepository.findById(id)
+				.orElseThrow();
+		return delete(customer);
+		
+		
+	}
+	
+	@CacheEvict(value =  "Customer", key = "#id")
+	public Customer delete(Customer customer) {
+		logger.info("Deleting customer {}", customer);
+		
+		customersRepository.delete(customer);
+		return customer;	
+	}
+
+	public List<Customer> get(Set<Sex> sexs) {
+		return customersRepository.findBySex(sexs);
+	}
+}
