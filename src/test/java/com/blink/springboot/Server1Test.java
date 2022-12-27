2d74dac0b24e9d62b5180f7ef5ca65e944ebd774
Sebastian Barrasa <sebastian.barrasa@blink-srl.com>
2022-12-27 16:47:02 -0300

    JUnit

--- src/test/java/com/blink/springboot/Server1Test.java
+++ src/test/java/com/blink/springboot/Server1Test.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.blink.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Server1Test {
 
 	@Test
 	void contextLoads() {
